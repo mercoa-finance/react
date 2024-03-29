40bc89af94c1f9756f846485c5d6461be10186b6
Sandeep Dinesh <sandeep@mercoa.com>
2024-03-29 16:45:20 -0400

    fix storybook padding

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,29 +1,33 @@
 import type { Preview } from '@storybook/react'
-import { InitializeOptions, initialize, mswLoader } from 'msw-storybook-addon'
+import { initialize, mswLoader } from 'msw-storybook-addon'
 import { MercoaSession } from '../src/components/Mercoa'
 import { mockToken, mswHandlers } from '../src/stories/mockData'
 
 import '../src/tailwind.base.css'
 import '../src/tailwind.css'
 
-let options: InitializeOptions = {
-  onUnhandledRequest: 'bypass',
-}
-if (location.hostname === 'mercoa-finance.github.io') {
-  options = {
-    ...options,
-    serviceWorker: {
-      url: '/react/mockServiceWorker.js',
-    },
-  }
-}
-initialize(options, mswHandlers)
+initialize(
+  {
+    onUnhandledRequest: 'bypass',
+  },
+  mswHandlers,
+)
 
 const preview: Preview = {
   parameters: {
     options: {
       sortStory: {
-        order: ['Portal', 'Invoice Inbox', 'Invoice', 'Entity', 'Payment Methods', 'Vendors', 'Approvals', 'Utility'],
+        order: [
+          'Drop-in Portal',
+          'Invoice Inbox',
+          ['Inbox', 'Invoice Table', 'Metrics'],
+          'Invoice',
+          'Entity',
+          'Payment Methods',
+          'Vendors',
+          'Approvals',
+          'Utility',
+        ],
       },
     },
     controls: {
