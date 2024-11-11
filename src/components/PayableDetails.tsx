fa5024650bbb4bd4e493afafc3e093cd713bc0ae
Sandeep Dinesh <sandeep@mercoa.com>
2024-11-10 21:07:07 -0800

    add zoom to image invoices

--- src/components/PayableDetails.tsx
+++ src/components/PayableDetails.tsx
@@ -1893,6 +1893,55 @@ export function PayableDocumentDisplay({
     setNumRenders(numRenders + 1)
   }, [debouncedWidth])
 
+  function getImageScale(scale: number) {
+    if (scale <= 0.5) return `mercoa-scale-[0.5]`
+    else if (scale <= 0.6) return `mercoa-scale-[0.6]`
+    else if (scale <= 0.7) return `mercoa-scale-[0.7]`
+    else if (scale <= 0.8) return `mercoa-scale-[0.8]`
+    else if (scale <= 0.9) return `mercoa-scale-[0.9]`
+    else if (scale <= 1) return `mercoa-scale-[1]`
+    else if (scale <= 1.1) return `mercoa-scale-[1.1]`
+    else if (scale <= 1.2) return `mercoa-scale-[1.2]`
+    else if (scale <= 1.3) return `mercoa-scale-[1.3]`
+    else if (scale <= 1.4) return `mercoa-scale-[1.4]`
+    else if (scale <= 1.5) return `mercoa-scale-[1.5]`
+    else if (scale <= 1.6) return `mercoa-scale-[1.6]`
+    else if (scale <= 1.7) return `mercoa-scale-[1.7]`
+    else if (scale <= 1.8) return `mercoa-scale-[1.8]`
+    else if (scale <= 1.9) return `mercoa-scale-[1.9]`
+    else if (scale <= 2) return `mercoa-scale-[2]`
+    else if (scale <= 2.1) return `mercoa-scale-[2.1]`
+    else if (scale <= 2.2) return `mercoa-scale-[2.2]`
+    else if (scale <= 2.3) return `mercoa-scale-[2.3]`
+    else if (scale <= 2.4) return `mercoa-scale-[2.4]`
+    else if (scale <= 2.5) return `mercoa-scale-[2.5]`
+    else if (scale <= 2.6) return `mercoa-scale-[2.6]`
+    else if (scale <= 2.7) return `mercoa-scale-[2.7]`
+    else if (scale <= 2.8) return `mercoa-scale-[2.8]`
+    else if (scale <= 2.9) return `mercoa-scale-[2.9]`
+    else if (scale <= 3) return `mercoa-scale-[3]`
+    else if (scale <= 3.1) return `mercoa-scale-[3.1]`
+    else if (scale <= 3.2) return `mercoa-scale-[3.2]`
+    else if (scale <= 3.3) return `mercoa-scale-[3.3]`
+    else if (scale <= 3.4) return `mercoa-scale-[3.4]`
+    else if (scale <= 3.5) return `mercoa-scale-[3.5]`
+    else if (scale <= 3.6) return `mercoa-scale-[3.6]`
+    else if (scale <= 3.7) return `mercoa-scale-[3.7]`
+    else if (scale <= 3.8) return `mercoa-scale-[3.8]`
+    else if (scale <= 3.9) return `mercoa-scale-[3.9]`
+    else if (scale <= 4) return `mercoa-scale-[4]`
+    else if (scale <= 4.1) return `mercoa-scale-[4.1]`
+    else if (scale <= 4.2) return `mercoa-scale-[4.2]`
+    else if (scale <= 4.3) return `mercoa-scale-[4.3]`
+    else if (scale <= 4.4) return `mercoa-scale-[4.4]`
+    else if (scale <= 4.5) return `mercoa-scale-[4.5]`
+    else if (scale <= 4.6) return `mercoa-scale-[4.6]`
+    else if (scale <= 4.7) return `mercoa-scale-[4.7]`
+    else if (scale <= 4.8) return `mercoa-scale-[4.8]`
+    else if (scale <= 4.9) return `mercoa-scale-[4.9]`
+    else if (scale <= 5) return `mercoa-scale-[5]`
+  }
+
   if (!mercoaSession.client) return <NoSession componentName="PayableDocumentDisplay" />
   if (!documents && !invoice)
     return <div>One of invoice or documents must be passed as a prop to PayableDocumentDisplay</div>
@@ -1902,91 +1951,89 @@ export function PayableDocumentDisplay({
         <div className="mercoa-w-full">
           {documents.map((document, i) => (
             <div key={i} className="mercoa-rounded-mercoa mercoa-border mercoa-shadow-lg mercoa-w-full">
-              {document.mimeType === 'application/pdf' && (
-                <div className="mercoa-grid mercoa-grid-cols-3 mercoa-px-1 mercoa-bg-gray-100 mercoa-border-b">
-                  <div className="mercoa-flex">
-                    <button
-                      type="button"
-                      disabled={pageNumber <= 1}
-                      onClick={() => setPageNumber(Math.max(pageNumber - 1, 1))}
-                      className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
-                    >
-                      <span className="mercoa-sr-only">Previous</span>
-                      <ChevronUpIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
-                    </button>
-                    <div className="mercoa-flex mercoa-items-center mercoa-justify-center mercoa-py-2 mercoa-gap-x-1 mercoa-text-gray-400">
-                      <input
-                        type="number"
-                        min={1}
-                        max={numPages ?? 1}
-                        value={pageNumber}
-                        onChange={(e) => setPageNumber(parseInt(e.target.value))}
-                        className={inputClassName({ width: 'mercoa-w-[50px]' })}
-                      />{' '}
-                      / {numPages}
-                    </div>
-                    <button
-                      type="button"
-                      disabled={pageNumber >= (numPages ?? 1)}
-                      onClick={() => setPageNumber(Math.min(pageNumber + 1, numPages ?? 1))}
-                      className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
-                    >
-                      <span className="mercoa-sr-only">Next</span>
-                      <ChevronDownIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
-                    </button>
+              <div className="mercoa-grid mercoa-grid-cols-3 mercoa-px-1 mercoa-bg-gray-100 mercoa-border-b">
+                <div className="mercoa-flex">
+                  <button
+                    type="button"
+                    disabled={pageNumber <= 1}
+                    onClick={() => setPageNumber(Math.max(pageNumber - 1, 1))}
+                    className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
+                  >
+                    <span className="mercoa-sr-only">Previous</span>
+                    <ChevronUpIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
+                  </button>
+                  <div className="mercoa-flex mercoa-items-center mercoa-justify-center mercoa-py-2 mercoa-gap-x-1 mercoa-text-gray-400">
+                    <input
+                      type="number"
+                      min={1}
+                      max={numPages ?? 1}
+                      value={pageNumber}
+                      onChange={(e) => setPageNumber(parseInt(e.target.value))}
+                      className={inputClassName({ width: 'mercoa-w-[50px]' })}
+                    />{' '}
+                    / {numPages}
                   </div>
-                  <div className="mercoa-flex mercoa-items-center mercoa-justify-center">
-                    <button
-                      type="button"
-                      disabled={zoomLevel === 0.1}
-                      onClick={() => setZoomLevel(Math.max(zoomLevel - 0.1, 0.1))}
-                      className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
-                    >
-                      <span className="mercoa-sr-only">Zoom Out</span>
-                      <MagnifyingGlassMinusIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
-                    </button>
-                    <div className="mercoa-flex mercoa-items-center mercoa-justify-center mercoa-py-2 mercoa-gap-x-1 mercoa-text-gray-400">
-                      <span>{Math.floor(zoomLevel * 100)}%</span>
-                    </div>
-                    <button
-                      type="button"
-                      disabled={zoomLevel === 5}
-                      onClick={() => setZoomLevel(Math.min(zoomLevel + 0.1, 5))}
-                      className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
-                    >
-                      <span className="mercoa-sr-only">Zoom In</span>
-                      <MagnifyingGlassPlusIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mercoa-flex mercoa-items-center mercoa-justify-end mercoa-gap-x-1">
-                    {onRemoveDocument ? (
-                      <div className="mercoa-cursor-pointer" onClick={onRemoveDocument}>
-                        <TrashIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" color="#6B7280" />
-                      </div>
-                    ) : null}
-                    <a
-                      href={document.fileReaderObj}
-                      target="_blank"
-                      rel="noreferrer"
-                      download
-                      className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
-                    >
-                      <span className="mercoa-sr-only">Download Invoice</span>
-                      <ArrowDownTrayIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
-                    </a>
-                    {showSourceEmail && sourceEmails && (
-                      <button
-                        type="button"
-                        onClick={() => setView('email')}
-                        className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
-                      >
-                        <span className="mercoa-sr-only">View Email</span>
-                        <EnvelopeIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
-                      </button>
-                    )}
-                  </div>
+                  <button
+                    type="button"
+                    disabled={pageNumber >= (numPages ?? 1)}
+                    onClick={() => setPageNumber(Math.min(pageNumber + 1, numPages ?? 1))}
+                    className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
+                  >
+                    <span className="mercoa-sr-only">Next</span>
+                    <ChevronDownIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
+                  </button>
                 </div>
-              )}
+                <div className="mercoa-flex mercoa-items-center mercoa-justify-center">
+                  <button
+                    type="button"
+                    disabled={zoomLevel === 0.1}
+                    onClick={() => setZoomLevel(Math.max(zoomLevel - 0.1, 0.1))}
+                    className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
+                  >
+                    <span className="mercoa-sr-only">Zoom Out</span>
+                    <MagnifyingGlassMinusIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
+                  </button>
+                  <div className="mercoa-flex mercoa-items-center mercoa-justify-center mercoa-py-2 mercoa-gap-x-1 mercoa-text-gray-400">
+                    <span>{Math.floor(zoomLevel * 100)}%</span>
+                  </div>
+                  <button
+                    type="button"
+                    disabled={zoomLevel === 5}
+                    onClick={() => setZoomLevel(Math.min(zoomLevel + 0.1, 5))}
+                    className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
+                  >
+                    <span className="mercoa-sr-only">Zoom In</span>
+                    <MagnifyingGlassPlusIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
+                  </button>
+                </div>
+                <div className="mercoa-flex mercoa-items-center mercoa-justify-end mercoa-gap-x-1">
+                  {onRemoveDocument ? (
+                    <div className="mercoa-cursor-pointer" onClick={onRemoveDocument}>
+                      <TrashIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" color="#6B7280" />
+                    </div>
+                  ) : null}
+                  <a
+                    href={document.fileReaderObj}
+                    target="_blank"
+                    rel="noreferrer"
+                    download
+                    className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
+                  >
+                    <span className="mercoa-sr-only">Download Invoice</span>
+                    <ArrowDownTrayIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
+                  </a>
+                  {showSourceEmail && sourceEmails && (
+                    <button
+                      type="button"
+                      onClick={() => setView('email')}
+                      className="mercoa-p-1 mercoa-text-gray-600 disabled:mercoa-text-gray-200"
+                    >
+                      <span className="mercoa-sr-only">View Email</span>
+                      <EnvelopeIcon className="mercoa-h-5 mercoa-w-5" aria-hidden="true" />
+                    </button>
+                  )}
+                </div>
+              </div>
               <div className="mercoa-overflow-x-auto ">
                 {document.mimeType === 'application/pdf' ? (
                   <Document
@@ -2021,7 +2068,14 @@ export function PayableDocumentDisplay({
                     />
                   </Document>
                 ) : (
-                  <img src={document.fileReaderObj} className="mercoa-object-contain mercoa-object-top mercoa-w-full" />
+                  <div className={`mercoa-w-full mercoa-overflow-scroll`}>
+                    <img
+                      src={document.fileReaderObj}
+                      className={`mercoa-origin-top-left mercoa-object-contain mercoa-object-top ${getImageScale(
+                        zoomLevel,
+                      )} mercoa-w-full`}
+                    />
+                  </div>
                 )}
               </div>
             </div>
