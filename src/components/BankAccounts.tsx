5f63ccbd448ce29cf44758bfaab45bc4245328b3
Sandeep Dinesh <sandeep@mercoa.com>
2025-01-28 17:07:01 -0800

    acceleration funds

--- src/components/BankAccounts.tsx
+++ src/components/BankAccounts.tsx
@@ -7,6 +7,7 @@ import {
   PencilIcon,
   PencilSquareIcon,
   PlusIcon,
+  WalletIcon,
 } from '@heroicons/react/24/outline'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Mercoa } from '@mercoa/javascript'
@@ -16,6 +17,7 @@ import { PlaidLinkError, PlaidLinkOnExitMetadata, usePlaidLink } from 'react-pla
 import SignatureCanvas from 'react-signature-canvas'
 import { toast } from 'react-toastify'
 import * as yup from 'yup'
+import { currencyCodeToSymbol } from '../lib/currency'
 import { capitalize } from '../lib/lib'
 import {
   AddDialog,
@@ -29,6 +31,7 @@ import {
   PaymentMethodList,
   Tooltip,
   inputClassName,
+  removeThousands,
   useMercoaSession,
 } from './index'
 
@@ -1078,5 +1081,332 @@ export function EditBankAccount({
         <MercoaButton isEmphasized>Update</MercoaButton>
       </div>
     </form>
+  )
+}
+
+export function AcceleratedAchFunds({
+  global,
+  entityId: passedEntityId,
+}: {
+  global?: boolean
+  entityId?: Mercoa.EntityId
+}) {
+  const mercoaSession = useMercoaSession()
+
+  const [balanceIsResolved, setBalanceIsResolved] = useState(false)
+  const [availableBalance, setAvailableBalance] = useState<number>(0)
+  const [pendingBalance, setPendingBalance] = useState<number>(0)
+  const [bankAccounts, setBankAccounts] = useState<Array<Mercoa.PaymentMethodResponse.BankAccount>>([])
+  const [selectedBankAccount, setSelectedBankAccount] = useState<Mercoa.PaymentMethodResponse.BankAccount>()
+  const [showDialog, setShowDialog] = useState<'add' | 'remove' | ''>('')
+
+  const entityId = passedEntityId ?? mercoaSession.entity?.id ?? mercoaSession.organization?.organizationEntityId
+
+  const schema = yup
+    .object({
+      amount: yup.number().transform(removeThousands).positive().typeError('Please enter a valid number'),
+    })
+    .required()
+
+  const addMethods = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      amount: 0,
+    },
+  })
+
+  const removeMethods = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      amount: 0,
+    },
+  })
+
+  // Consolidated useEffect for fetching bank accounts and acceleration funds
+  useEffect(() => {
+    const fetchBankAccountsAndFunds = async () => {
+      if (!mercoaSession.token || !entityId || !mercoaSession.client) return
+      try {
+        // Fetch bank accounts
+        const resp = await mercoaSession.client.entity.paymentMethod.getAll(entityId, { type: 'bankAccount' })
+        const bankAccountPms = resp.filter(
+          (e) => e.type === 'bankAccount' && e.status === 'VERIFIED',
+        ) as Array<Mercoa.PaymentMethodResponse.BankAccount>
+        const firstBankAccount = bankAccountPms[0]
+        setBankAccounts(bankAccountPms)
+        setSelectedBankAccount(firstBankAccount)
+
+        // Fetch acceleration funds if a bank account exists
+        if (firstBankAccount) {
+          const fundsResp = await mercoaSession.client.entity.paymentMethod.bankAccount.getAccelerationFunds(
+            entityId,
+            firstBankAccount.id,
+          )
+          setAvailableBalance(fundsResp.availableBalance.amount)
+          setPendingBalance(fundsResp.pendingBalance.amount)
+          setBalanceIsResolved(true)
+        }
+      } catch (error) {
+        console.error('Error fetching bank accounts or acceleration funds:', error)
+      }
+    }
+
+    fetchBankAccountsAndFunds()
+  }, [mercoaSession.client, entityId, mercoaSession.token])
+
+  // Add acceleration funds
+  const handleAddFunds = async (data: { amount?: number }) => {
+    if (!data.amount) return
+    if (!confirm('Are you sure you want to add funds?')) return
+    if (!mercoaSession.token || !entityId || !mercoaSession.client || !balanceIsResolved) {
+      toast.error('Failed to resolve acceleration funds')
+      return
+    }
+    if (!selectedBankAccount?.id) {
+      toast.error('No bank account selected')
+      return
+    }
+    try {
+      await mercoaSession.client.entity.paymentMethod.bankAccount.addAccelerationFunds(
+        entityId,
+        selectedBankAccount.id,
+        { amount: data.amount, currency: Mercoa.CurrencyCode.Usd },
+      )
+    } catch (e) {
+      toast.error('Failed to add acceleration funds')
+      return
+    }
+    // Keep local state in sync with acceleration funds
+    setPendingBalance((prevBalance) => prevBalance + (data?.amount ?? 0))
+    addMethods.setValue('amount', 0)
+  }
+
+  // Remove acceleration funds
+  const handleRemoveFunds = async (data: { amount?: number }) => {
+    if (!data.amount) return
+    if (!confirm('Are you sure you want to remove funds?')) return
+    if (!mercoaSession.token || !entityId || !mercoaSession.client || !balanceIsResolved) {
+      toast.error('Failed to resolve acceleration funds')
+      return
+    }
+    if (!selectedBankAccount?.id) {
+      toast.error('No bank account selected')
+      return
+    }
+    try {
+      await mercoaSession.client.entity.paymentMethod.bankAccount.removeAccelerationFunds(
+        entityId,
+        selectedBankAccount.id,
+        { amount: data.amount, currency: Mercoa.CurrencyCode.Usd },
+      )
+    } catch (e) {
+      toast.error('Failed to remove acceleration funds')
+      return
+    }
+    // Keep local state in sync with acceleration funds
+    setAvailableBalance((prevBalance) => prevBalance - (data?.amount ?? 0))
+    removeMethods.setValue('amount', 0)
+  }
+
+  if (!entityId) {
+    return (
+      <div className="mercoa-px-4 sm:mercoa-px-6 lg:mercoa-px-8 mercoa-pb-8">
+        <div className="sm:mercoa-flex sm:mercoa-items-center">
+          <div className="sm:mercoa-flex-auto">
+            <h1 className="mercoa-text-xl mercoa-font-semibold mercoa-text-gray-900">
+              {global ? 'Global' : ''} Accelerated ACH - Disabled
+            </h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="mercoa-px-4 sm:mercoa-px-6 lg:mercoa-px-8 mercoa-pb-8">
+      <div className="sm:mercoa-flex sm:mercoa-items-center">
+        <div className="sm:mercoa-flex-auto">
+          <h1 className="mercoa-text-xl mercoa-font-semibold mercoa-text-gray-900">
+            {global ? 'Global' : ''} Accelerated ACH
+          </h1>
+
+          {/* Current Acceleration Funds Balance */}
+          <div className="mercoa-mt-8">
+            <div className="mercoa-grid mercoa-grid-cols-4 mercoa-gap-2">
+              <div className="mercoa-flex mercoa-flex-col mercoa-items-center mercoa-justify-center">
+                <h2 className="mercoa-block mercoa-text-left mercoa-text-2xl mercoa-font-medium mercoa-text-gray-700">
+                  {balanceIsResolved ? `$${availableBalance.toFixed(2)}` : '--'}
+                </h2>
+                <div className="mercoa-text-sm">Available Funds</div>
+              </div>
+              <div className="mercoa-flex mercoa-flex-col mercoa-items-center mercoa-justify-center">
+                <h2 className="mercoa-block mercoa-text-left mercoa-text-2xl mercoa-font-medium mercoa-text-gray-700">
+                  {balanceIsResolved ? `$${pendingBalance.toFixed(2)}` : '--'}
+                </h2>
+                <div className="mercoa-text-sm">Pending Funds</div>
+              </div>
+              <MercoaButton color="indigo" isEmphasized={false} onClick={() => setShowDialog('add')}>
+                Add Funds
+              </MercoaButton>
+              <MercoaButton color="red" isEmphasized={false} onClick={() => setShowDialog('remove')}>
+                Withdraw Funds
+              </MercoaButton>
+              <Transition.Root show={!!showDialog} as={Fragment}>
+                <Dialog as="div" className="mercoa-relative mercoa-z-10" onClose={() => setShowDialog('')}>
+                  <Transition.Child
+                    as={Fragment}
+                    enter="mercoa-ease-out mercoa-duration-300"
+                    enterFrom="mercoa-opacity-0"
+                    enterTo="mercoa-opacity-100"
+                    leave="mercoa-ease-in mercoa-duration-200"
+                    leaveFrom="mercoa-opacity-100"
+                    leaveTo="mercoa-opacity-0"
+                  >
+                    <div className="mercoa-fixed mercoa-inset-0 mercoa-bg-gray-500 mercoa-bg-mercoa-opacity-75 mercoa-transition-opacity" />
+                  </Transition.Child>
+
+                  <div className="mercoa-fixed mercoa-inset-0 mercoa-z-10 mercoa-overflow-y-auto">
+                    <div className="mercoa-flex mercoa-min-h-full mercoa-items-end mercoa-justify-center mercoa-p-4 mercoa-text-center sm:mercoa-items-center sm:mercoa-p-0">
+                      <Transition.Child
+                        as={Fragment}
+                        enter="mercoa-ease-out mercoa-duration-300"
+                        enterFrom="mercoa-opacity-0 mercoa-translate-y-4 sm:mercoa-translate-y-0 sm:mercoa-scale-95"
+                        enterTo="mercoa-opacity-100 mercoa-translate-y-0 sm:mercoa-scale-100"
+                        leave="mercoa-ease-in mercoa-duration-200"
+                        leaveFrom="mercoa-opacity-100 mercoa-translate-y-0 sm:mercoa-scale-100"
+                        leaveTo="mercoa-opacity-0 mercoa-translate-y-4 sm:mercoa-translate-y-0 sm:mercoa-scale-95"
+                      >
+                        <Dialog.Panel className="mercoa-relative mercoa-transform mercoa-rounded-mercoa mercoa-bg-white mercoa-px-4 mercoa-pt-5 mercoa-pb-4 mercoa-text-left mercoa-shadow-xl mercoa-transition-all sm:mercoa-my-8 sm:mercoa-w-full sm:mercoa-max-w-lg sm:mercoa-p-6">
+                          <Dialog.Title
+                            as="h3"
+                            className="mercoa-text-lg mercoa-font-medium mercoa-leading-6 mercoa-text-gray-900"
+                          >
+                            Transfer Funds
+                          </Dialog.Title>
+
+                          {showDialog === 'remove' && (
+                            <>
+                              <h2 className="mercoa-block mercoa-text-left mercoa-font-medium mercoa-text-gray-900 mercoa-mt-4">
+                                Transfer From
+                              </h2>
+                              <div className="mercoa-mt-1">
+                                <PaymentMethodButton
+                                  selected
+                                  icon={<WalletIcon className="mercoa-size-5" />}
+                                  text={'Acceleration Wallet'}
+                                />
+                              </div>
+                            </>
+                          )}
+
+                          {/* Select Source Bank Account */}
+                          <div className="mercoa-my-4">
+                            <h2 className="mercoa-block mercoa-text-left mercoa-font-medium mercoa-text-gray-900">
+                              {showDialog === 'add' ? 'Transfer From' : 'To'}
+                            </h2>
+                            <div className="mercoa-mt-1 mercoa-overflow-y-auto mercoa-h-48 mercoa-p-3 mercoa-border mercoa-border-gray-200 mercoa-rounded-md">
+                              {bankAccounts.map((bankAccount) => (
+                                <div key={bankAccount.id} className="mercoa-mt-1">
+                                  <BankAccount
+                                    account={bankAccount}
+                                    selected={bankAccount.id === selectedBankAccount?.id}
+                                    onSelect={() => {
+                                      setSelectedBankAccount(bankAccount)
+                                    }}
+                                  />
+                                </div>
+                              ))}
+                            </div>
+
+                            {bankAccounts.length === 0 && (
+                              <div className="mercoa-mt-2 mercoa-text-left mercoa-text-gray-700">
+                                No verified bank accounts found. Please add and verify at least one bank account.
+                              </div>
+                            )}
+
+                            {showDialog === 'add' && (
+                              <>
+                                <h2 className="mercoa-block mercoa-text-left mercoa-font-medium mercoa-text-gray-900 mercoa-mt-4">
+                                  To
+                                </h2>
+                                <div className="mercoa-mt-1">
+                                  <PaymentMethodButton
+                                    selected
+                                    icon={<WalletIcon className="mercoa-size-5" />}
+                                    text={'Acceleration Wallet'}
+                                  />
+                                </div>
+                              </>
+                            )}
+                          </div>
+
+                          {showDialog === 'add' ? (
+                            <form
+                              className="mercoa-p-3 mercoa-rounded-md mercoa-bg-gray-200 mercoa-flex mercoa-items-end mercoa-gap-x-4"
+                              onSubmit={addMethods.handleSubmit(handleAddFunds)}
+                            >
+                              <MercoaInput
+                                control={addMethods.control}
+                                name="amount"
+                                label="Amount"
+                                type="currency"
+                                className="md:mercoa-col-span-1 mercoa-col-span-full"
+                                leadingIcon={
+                                  <span className="mercoa-text-gray-500 sm:mercoa-text-sm">
+                                    {currencyCodeToSymbol(Mercoa.CurrencyCode.Usd)}
+                                  </span>
+                                }
+                                errors={addMethods.formState.errors}
+                              />
+
+                              <MercoaButton
+                                isEmphasized
+                                type="submit"
+                                className="mercoa-mt-4"
+                                disabled={!balanceIsResolved}
+                              >
+                                Transfer
+                              </MercoaButton>
+                            </form>
+                          ) : (
+                            <form
+                              className="mercoa-p-3 mercoa-rounded-md mercoa-bg-gray-200 mercoa-flex mercoa-items-end mercoa-gap-x-4"
+                              onSubmit={removeMethods.handleSubmit(handleRemoveFunds)}
+                            >
+                              <MercoaInput
+                                control={removeMethods.control}
+                                name="amount"
+                                label="Amount"
+                                type="currency"
+                                className="md:mercoa-col-span-1 mercoa-col-span-full"
+                                leadingIcon={
+                                  <span className="mercoa-text-gray-500 sm:mercoa-text-sm">
+                                    {currencyCodeToSymbol(Mercoa.CurrencyCode.Usd)}
+                                  </span>
+                                }
+                                errors={removeMethods.formState.errors}
+                              />
+
+                              <MercoaButton
+                                isEmphasized
+                                type="submit"
+                                className="mercoa-mt-4"
+                                disabled={!balanceIsResolved}
+                              >
+                                Transfer
+                              </MercoaButton>
+                            </form>
+                          )}
+                        </Dialog.Panel>
+                      </Transition.Child>
+                    </div>
+                  </div>
+                </Dialog>
+              </Transition.Root>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
